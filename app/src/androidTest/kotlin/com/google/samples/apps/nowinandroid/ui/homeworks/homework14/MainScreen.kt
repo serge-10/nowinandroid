b878f905e896332a8498360fb6bfd088ad678ebe
Sergei <vlasovseverin@gmail.com>
2025-03-20 17:38:34 +0100

    классная работа 14

--- app/src/androidTest/kotlin/com/google/samples/apps/nowinandroid/ui/homeworks/homework14/MainScreen.kt
+++ app/src/androidTest/kotlin/com/google/samples/apps/nowinandroid/ui/homeworks/homework14/MainScreen.kt
@@ -17,15 +17,26 @@
 package com.google.samples.apps.nowinandroid.ui.homeworks.homework14
 
 import androidx.compose.ui.test.SemanticsNodeInteractionsProvider
-import io.github.kakaocup.compose.node.builder.NodeMatcher
+import com.kaspersky.components.composesupport.core.KNode
 import io.github.kakaocup.compose.node.element.ComposeScreen
-import io.github.kakaocup.compose.node.element.KNode
 
-class MainScreen(semanticsProvider: SemanticsNodeInteractionsProvider) :
-    ComposeScreen<MainScreen>(semanticsProvider) {
+class MainScreen(semanticProvides: SemanticsNodeInteractionsProvider) :
+    ComposeScreen<MainScreen>(semanticProvides) {
 
-        val donebutton = KNode(semanticsProvider){
+    val doneButton = KNode(semanticProvides) {
+        hasText("Done")
+    }
 
+    val headlinesIcon = KNode(semanticProvides) {
+        hasContentDescription("Headlines")
 
-        }
+    }
+
+    val uiIcon = KNode(semanticProvides) {
+        hasContentDescription("UI")
+
+    }
+    val composeIcon = KNode(semanticProvides) {
+        hasContentDescription("Compose")
+    }
 }
